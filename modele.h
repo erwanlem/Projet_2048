fe9dfe10cab398b857e3100ab70785c61eea0bdc
erwanlem <erwan.lemattre@hotmail.com>
2021-11-17 22:54:56 +0100

    Final niv0
    Organisation des fonctions dans 2048.cpp, modele.h et modele.cpp

--- modele.h
+++ modele.h
@@ -1,12 +1,16 @@
 #include <vector>
 using namespace std;
 
-const int GAUCHE = 7, DROITE = 4, HAUT = 8, BAS = 2;
+extern int QUATRE;
+
+const int GAUCHE = 113, DROITE = 100, HAUT = 122, BAS = 115;
 typedef vector<vector<int>> Plateau;
+
 
 Plateau plateauVide();
 Plateau plateauInitial();
 int tireDeuxOuQuatre();
+Plateau ajouteDeuxOuQuatre(Plateau plateau);
 
 Plateau deplacementGauche(Plateau plateau);
 Plateau deplacementDroite(Plateau plateau);
@@ -14,8 +18,18 @@ Plateau deplacementHaut(Plateau plateau);
 Plateau deplacementBas(Plateau plateau);
 Plateau deplacement(Plateau plateau, int direction);
 
-string dessine(Plateau g);
+void dessine(Plateau plateau);
 bool estTermine(Plateau plateau);
 bool estGagnant(Plateau plateau);
 int score(Plateau plateau);
+bool estRempli(Plateau plateau);
 
+
+// Les fonctions de test
+void testTireDeuxOuQuatre();
+void testAjouteDeuxOuQuatre();
+void testDeplacementGauche();
+void testDeplacementDroite();
+void testDeplacementHaut();
+void testDeplacementBas();
+void testDeplacement();
