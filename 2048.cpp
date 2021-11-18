dc6588640f99a6d95abf39e14e049af960e9bd44
erwanlem <erwan.lemattre@hotmail.com>
2021-11-18 15:21:50 +0100

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -1,31 +1,12 @@
-#include <vector>
 #include <iostream>
+#include <time.h>
 #include "modele.h"
 
 using namespace std;
 
-const int GAUCHE = 7, DROITE = 4, HAUT = 8, BAS = 2;
-typedef vector<vector<int>> Plateau;
-
-Plateau deplacement(Plateau plateau, int direction) {
-  switch ( direction ) {
-    case GAUCHE:
-      return deplacementGauche(plateau);
-    case DROITE:
-      return deplacementDroite(plateau);
-    case HAUT:
-      return deplacementHaut(plateau);
-    case BAS:
-      return deplacementBas(plateau);
-    default:
-      cerr << "Deplacement non-autorise!" << endl;
-      exit(-1);
-  }
-}
 
 int main(int argc, char const *argv[])
 {
-    testEstTermine();
     // Initialisation du temps pour l'aléatoire
     srand((int)time(0));
 
@@ -37,7 +18,7 @@ int main(int argc, char const *argv[])
     do
     {
         plateau = plateauVide();
-        plateau = plateauInitial(plateau);
+        plateau = plateauInitial();
         s = 0;
         quatre  = 0;
         while ( !estGagnant(plateau) &&  !estTermine(plateau) ){
