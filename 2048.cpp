2428a74b5d2ff678f88c852bf30fcb4cb4267cff
erwanlem <94453013+erwanlem@users.noreply.github.com>
2021-12-05 15:22:07 +0100

    Update_05/12

--- 2048.cpp
+++ 2048.cpp
@@ -3,12 +3,14 @@
 #include "modele.h"
 #include <ncurses.h>
 
+
 using namespace std;
 
 
 
 int main(int argc, char const *argv[])
 {
+    testEstGagnant();
     // Initialisation du temps pour l'aléatoire
     srand((int)time(0));
 
@@ -22,11 +24,18 @@ int main(int argc, char const *argv[])
     Plateau lastPlateau;
 
     do
-    {
+    {   
+        init_pair(10, COLOR_BLUE, COLOR_BLACK);
+        init_pair(11, COLOR_RED, COLOR_BLACK);
         plateau = plateauInitial();
         while ( !estGagnant(plateau.plateau) &&  !estTermine(plateau.plateau) ){
             clear();
-            printw("\nUtiliser les flèches pour jouer\nCliquer sur toute autre touche pour quitter\n\n\n\tScore: %d\n\n",score(plateau));
+            attron(COLOR_PAIR(10));
+            printw("\n\n ################## 2048 ##################\n #### Utiliser les flèches pour jouer #####\n #### Cliquer sur echap pour quitter  #####");
+            attroff(COLOR_PAIR(10));
+            attron(COLOR_PAIR(11));
+            printw("\n\n\n\tScore: %d\n\n",score(plateau));
+            attroff(COLOR_PAIR(11));
             dessine(plateau.plateau);
             touche = getch();
             lastPlateau = plateau.plateau;
@@ -38,7 +47,7 @@ int main(int argc, char const *argv[])
        }
        clear();
        dessine(plateau.plateau);
-       if (estGagnant(plateau.plateau)){
+       if (estGagnant(plateau.plateau) && estTermine(plateau.plateau)){
            printw("\n\n\tVous avez gagné !\nVous pouvez continuer de jouer...\n");
        } else {
            printw("\nOups... Vous avez perdu\n");
